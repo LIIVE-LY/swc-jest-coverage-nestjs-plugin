3fbd3410da194410e3fedd7c99581ee88e1cc7fd
Daniel Morley <daniel@liive.ly>
2026-02-15 08:46:42 +0000

    test(Fixtures): NestJS resolve fields

---
+++ tests/fixture/resolve_field/output.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _graphql = require("@nestjs/graphql");
+function _ts_decorate(decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+}
+function _ts_metadata(k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+}
+function _ts_param(paramIndex, decorator) {
+    return function(target, key) {
+        decorator(target, key, paramIndex);
+    };
+}
+class MenuResolver {
+    status(menu) {
+        return 'active';
+    }
+    async getDiscounts(menu) {
+        return [];
+    }
+}
+_ts_decorate([
+    (0, _graphql.ResolveField)(String),
+    _ts_param(0, (0, _graphql.Parent)())
+], MenuResolver.prototype, "status", null);
+_ts_decorate([
+    (0, _graphql.ResolveField)('discounts', [
+            String
+        ]),
+    _ts_param(0, (0, _graphql.Parent)())
+], MenuResolver.prototype, "getDiscounts", null);
