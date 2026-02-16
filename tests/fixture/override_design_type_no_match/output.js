479f6870e5a57b8ff8c71cce84b5b0d0f289e857
Daniel Morley <daniel@liive.ly>
2026-02-16 06:13:51 +0000

    test(Fixtures): Add override design:type transform fixtures for match and no-match

---
+++ tests/fixture/override_design_type_no_match/output.js
@@ -0,0 +1,4 @@
+_ts_decorate([
+    (0, _mongoose.Prop)(),
+    _ts_metadata("design:type", typeof mongoose === "undefined" || typeof mongoose.Types === "undefined" || typeof mongoose.Types.ObjectId === "undefined" ? Object : mongoose.Types.ObjectId)
+], VenueModel.prototype, "venueId", void 0);
